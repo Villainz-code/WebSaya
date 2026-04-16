8a52ac0cc230b4761f8c6ab53fecbafdeebfac61
Villainz-code <ahmadnurrafiu@gmail.com>
2026-04-16 15:58:22 +0700

    first commit

---
+++ views/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Heading / Judul -->
+    <h1>Belajar HTML</h1>
+    <h2>Belajar HTML</h2>
+    <h3>Belajar HTML</h3>
+    <h4>Belajar HTML</h4>
+    <h5>Belajar HTML</h5>
+    <h6>Belajar HTML</h6>
+
+    <hr>
+    <!-- Paragraf -->
+
+    <p align="center">Ini adalah paragraf pembuka </p>
+
+    <b>bold teks </b>
+    <i> italian teks</i>
+    <u> underline teks</u>
+    <strike> strike teks</strike>
+    <mark> mark teks</mark>
+    <small> small teks</small>
+    <big> big teks</big>
+    <hr><br>
+
+    <!-- link -->
+
+    <a href="https://www.instagram.com/ahmdnrrfii">Instagram</a>
+
+    <!-- list -->
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ul>
+
+    <ol>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ol>
+
+    <!-- table -->
+    <table>
+        <tr>
+            <td>item 1</td>
+            <td>item 2</td>
+            <td>item 3</td>
+        </tr>
+    </table>
+
+    <!-- image -->
+    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png" alt="Google">
+    <img src="https://freelogopng.com/images/all_img/1658588965instagram-logo-png-transparent-background.png"
+        alt="Instagram">
+</body>
+
+</html>
